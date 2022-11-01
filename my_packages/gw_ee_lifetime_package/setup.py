af2a445bc03972db5091cb8343dcc8502b229998
Brian Robinson <b.p.robinson102@gmail.com>
2022-11-01 17:31:19 -0500

    update

--- my_packages/gw_ee_lifetime_package/setup.py
+++
@@ -1,9 +0,0 @@
-from glob import glob
-from os.path import basename, splitext
-from setuptools import find_packages, setup
-
-setup(
-    name='gw_ee_lifetime_package_files',
-    version='0.1.0',
-    packages=find_packages(),
-)
