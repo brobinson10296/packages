9a0f5111358631e24c52347ef851855b44a63ea2
Brian Robinson <b.p.robinson102@gmail.com>
2022-11-02 13:03:40 -0500

    update

---
+++ fermi_fit_package/setup.py
@@ -0,0 +1,9 @@
+from glob import glob
+from os.path import basename, splitext
+from setuptools import find_packages, setup
+
+setup(
+    name='fermi_fit',
+    version='0.1.0',
+    packages=find_packages(),
+)
